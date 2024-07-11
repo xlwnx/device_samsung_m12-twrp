ec4e14d8d61b770e94acf61b5590e0d5c1410e02
Your Name <you@example.com>
2024-07-11 23:59:47 +0000

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m12.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m12-user \
+    omni_m12-userdebug \
+    omni_m12-eng
