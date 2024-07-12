9c0e03af3986709fb4d6625fdf22734d61f17ba5
xlwnx <105181270+xlwnx@users.noreply.github.com>
2024-07-12 21:09:22 +0700

    Update and rename omni_m12.mk to twrp_m12.mk

--- twrp_m12.mk
+++ twrp_m12.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/samsung/m12/device.mk)
 
 PRODUCT_DEVICE := m12
-PRODUCT_NAME := omni_m12
+PRODUCT_NAME := twrp_m12
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M127F
 PRODUCT_MANUFACTURER := samsung
